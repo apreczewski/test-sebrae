72331cf95c2a47eb7b9f06eb7b86f83e05f7c2df
Alexandre Preczewski <apreczewski@gmail.com>
2023-05-17 15:58:28 -0300

    create navbar global

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
     appDir: true,
     serverActions: true,
   },
-}
+  images: {
+    domains: ["images.unsplash.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
